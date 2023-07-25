552a14168b6a66730e49487889edbb8685681936
GeorgiKostadinovv <>
2023-07-25 14:41:10 +0000

    Add questions structure inside js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,80 @@
+const questions = [
+    {
+    question: "How much is 1 + 1?",
+    answers: [
+        {text:"22", correct: false},
+        {text:"11", correct: false},
+        {text:"4", correct: false},
+        {text:"2", correct: true},
+    ]
+    },
+    {
+    question: "How much is 2 + 1?",
+    answers: [
+        {text:"22", correct: false},
+        {text:"3", correct: true},
+        {text:"4", correct: false},
+        {text:"2", correct: false},
+    ]
+    },
+    {
+    question: "How much is 3 + 2?",
+    answers: [
+        {text:"5", correct: true},
+        {text:"11", correct: false},
+        {text:"4", correct: false},
+        {text:"2", correct: false},
+    ]
+    },
+    {
+    question: "How much is 3 + 3?",
+    answers: [
+        {text:"6", correct: true},
+        {text:"11", correct: false},
+        {text:"4", correct: false},
+        {text:"2", correct: false},
+    ]
+    },
+    {
+    question: "How much is 4 + 4?",
+    answers: [
+        {text:"22", correct: false},
+        {text:"11", correct: false},
+        {text:"4", correct: false},
+        {text:"8", correct: true},
+    ]
+    },
+    {
+    question: "How much is 7 + 7?",
+    answers: [
+        {text:"22", correct: false},
+        {text:"11", correct: false},
+        {text:"14", correct: true},
+        {text:"2", correct: false},
+    ]
+    },
+    {
+    question: "How much is 10 + 0?",
+    answers: [
+        {text:"22", correct: false},
+        {text:"11", correct: false},
+        {text:"10", correct: true},
+        {text:"2", correct: false},
+    ]
+    },
+    {
+    question: "How much is 0 + 0?",
+    answers: [
+        {text:"22", correct: false},
+        {text:"11", correct: false},
+        {text:"4", correct: false},
+        {text:"0", correct: true},
+    ]
+    },
+];
+
+
+
 runGame ()
 
 displayQuestion()
