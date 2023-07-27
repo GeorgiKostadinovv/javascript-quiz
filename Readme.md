b0a80da88251b732aca91d232296fa6328aad097
GeorgiKostadinovv <>
2023-07-27 21:49:31 +0000

    Add content top readme file

--- Readme.md
+++ Readme.md
@@ -1,5 +1,109 @@
+# QuiZ World
+
+Quiz World is a web site in construction that aims to atract people who love to play quiz. At the moment we are working on the beta version and we only have implemented our basic math quiz. Next step is going to be the implementation of more sections like:
+
+- History
+- Sports
+- Movies
+- Discworld (Terry Pratchet)
+- And much more...
+
+Once we acomplish that, we`ll create a competition with amazing rewards every week!
+
 ## Features
 
 ### Navigation Bar
 
+- The responisive Navigation bar is displayed in each page and contains links with text and icons to all four sections: Welcome, Game, Contact Us and Rules. This way it will allow fast access to every page without any need to go back to the previous one.
+
 ![navbar](/assets/images/navbar.png)
+
+### Landing Page
+
+- This is the first page the user will see, it has a welcoming message and an picture of an interrogation, very commonly related to quiz games. The picture is a link to the quiz or game section.
+
+![landingpage](/assets/images/landingpage.png)
+
+### Quiz/Game Section
+
+- This sections is where the game has place. The user will be asked a simple math question and will be offered four different answers with only one correct.
+
+![gamesection](/assets/images/gamesection.png)
+
+- Once the user select an answer, if the same is the correct one, the button will change to green color. If, by other hand, is incorret then the button will change to red color. 
+
+- Also, the Next button will be displayed on screen and the rest of buttons will be disabled.
+
+![gamesection](/assets/images/gamesection2.png)
+
+- At the end of the game the user will be informed about the final score.
+
+- The next button will change to Replay.
+
+![gamescore](/assets/images/gamescore.png)
+
+### Contact Us
+
+- This section contains a form that will allow users to contact us simply by leaving their name and email, they can also leave a comment.
+
+![contactUs](/assets/images/contactUs.png)
+
+### Rules Section
+
+- This page contains the rules of the quiz.
+
+![rules](/assets/images/rules.png)
+
+### Next steps...
+
+- As mentioned before, our plan is to implement more quiz games and updating with more and more categories.
+
+- Once the first step is done, we want to organise weekly competitions with rewards and more. For that will be necessary to include a profile system where every user can register and store their score. 
+
+## Testing
+
+- Navigation Bar: It`s fully responsive and every link works. Names and icons of the links are clear and allow an easy use of the bar.
+
+- Landing page: The picture and the previews explanation make easy to understand how to start the game. The picture is not adjusting properly below 412px.
+
+- Quiz/Game section: It is a simple interface that allows very easy use, comntains one question and below, four different answers. At the beginning we included a Preview button but at the end was discarted because of no need for it. Once answered, with the pressing of the Next button, user will be shonw the next question. It has a color system to inform if the answer is correct or incorrect.
+
+- Contact us: Responsive contact form that doesen`t require more than the basic contact information plus feedback.
+
+## Validator Testing
+
+- HTML
+No errors were returned when passing through the official W3C validator (https://validator.w3.org/).
+
+- CSS
+No errors were found when passing through the official (Jigsaw) validator (https://jigsaw.w3.org/css-validator/validator).
+
+- Javascript
+This are the errors found when passing through the validator (https://beautifytools.com/javascript-validator.php)
+
+![errors](/assets/images/errors.png)
+
+## Documentation
+
+- Pictures were taken from [pexels](https://www.pexels.com/es-es/)
+
+- Icons were taken from [Font Awesome](https://fontawesome.com/)
+
+- Readme file elaborated with [readme](https://readme.so/)
+
+- Images compressed with [compressjpeg](https://compressjpeg.com/)
+
+- For HTML/CSS and Javascript the sources have been used:
+
+   - [w3school](https://www.w3schools.com/)
+   - [freecodecamp](https://www.freecodecamp.org/)
+   - [GreatStackDev](https://www.youtube.com/@GreatStackDev)
+   - [JamesQQuick](https://www.youtube.com/@JamesQQuick)
+   - [briandesign](https://www.youtube.com/@briandesign)
+   - [Kevin Powell](https://www.youtube.com/@KevinPowell)
+
+   ## Deployment
+
+- The site was deployed to GitHub pages. The steps to deploy are as follows:
+- In the GitHub repository, navigate to the Settings tab From the source section drop-down menu.
+- Select the Master Branch.  
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,79 +1,126 @@
 /* Variable that stores all the questions, possible answers and correct one*/
 
-const questions = [
-    {
-        question: "How much is 1 + 1?",
-        answers: [
-            {text:"22", correct: false},
-            {text:"11", correct: false},
-            {text:"4", correct: false},
-            {text:"2", correct: true},
-        ]
-    },
-    {
-        question: "How much is 2 + 1?",
-        answers: [
-            {text:"22", correct: false},
-            {text:"3", correct: true},
-            {text:"4", correct: false},
-            {text:"2", correct: false},
-        ]
-    },
-    {
-        question: "How much is 3 + 2?",
-        answers: [
-            {text:"5", correct: true},
-            {text:"11", correct: false},
-            {text:"4", correct: false},
-            {text:"2", correct: false},
-        ]
-    },
-    {
-        question: "How much is 3 + 3?",
-        answers: [
-            {text:"6", correct: true},
-            {text:"11", correct: false},
-            {text:"4", correct: false},
-            {text:"2", correct: false},
-        ]
-    },
-    {
-        question: "How much is 4 + 4?",
-        answers: [
-            {text:"22", correct: false},
-            {text:"11", correct: false},
-            {text:"4", correct: false},
-            {text:"8", correct: true},
-        ]
-    },
-    {
-        question: "How much is 7 + 7?",
-        answers: [
-            {text:"22", correct: false},
-            {text:"11", correct: false},
-            {text:"14", correct: true},
-            {text:"2", correct: false},
-        ]
-    },
-    {
-        question: "How much is 10 + 0?",
-        answers: [
-            {text:"22", correct: false},
-            {text:"11", correct: false},
-            {text:"10", correct: true},
-            {text:"2", correct: false},
-        ]
-    },
-    {
-        question: "How much is 0 + 0?",
-        answers: [
-            {text:"22", correct: false},
-            {text:"11", correct: false},
-            {text:"4", correct: false},
-            {text:"0", correct: true},
-        ]
-    },
-]
+const questions = [{
+	question: "How much is 1 + 1?",
+	answers: [{
+		text: "22",
+		correct: false
+	}, {
+		text: "11",
+		correct: false
+	}, {
+		text: "4",
+		correct: false
+	}, {
+		text: "2",
+		correct: true
+	}, ]
+}, {
+	question: "How much is 2 + 1?",
+	answers: [{
+		text: "22",
+		correct: false
+	}, {
+		text: "3",
+		correct: true
+	}, {
+		text: "4",
+		correct: false
+	}, {
+		text: "2",
+		correct: false
+	}, ]
+}, {
+	question: "How much is 3 + 2?",
+	answers: [{
+		text: "5",
+		correct: true
+	}, {
+		text: "11",
+		correct: false
+	}, {
+		text: "4",
+		correct: false
+	}, {
+		text: "2",
+		correct: false
+	}, ]
+}, {
+	question: "How much is 3 + 3?",
+	answers: [{
+		text: "6",
+		correct: true
+	}, {
+		text: "11",
+		correct: false
+	}, {
+		text: "4",
+		correct: false
+	}, {
+		text: "2",
+		correct: false
+	}, ]
+}, {
+	question: "How much is 4 + 4?",
+	answers: [{
+		text: "22",
+		correct: false
+	}, {
+		text: "11",
+		correct: false
+	}, {
+		text: "4",
+		correct: false
+	}, {
+		text: "8",
+		correct: true
+	}, ]
+}, {
+	question: "How much is 7 + 7?",
+	answers: [{
+		text: "22",
+		correct: false
+	}, {
+		text: "11",
+		correct: false
+	}, {
+		text: "14",
+		correct: true
+	}, {
+		text: "2",
+		correct: false
+	}, ]
+}, {
+	question: "How much is 10 + 0?",
+	answers: [{
+		text: "22",
+		correct: false
+	}, {
+		text: "11",
+		correct: false
+	}, {
+		text: "10",
+		correct: true
+	}, {
+		text: "2",
+		correct: false
+	}, ]
+}, {
+	question: "How much is 0 + 0?",
+	answers: [{
+		text: "22",
+		correct: false
+	}, {
+		text: "11",
+		correct: false
+	}, {
+		text: "4",
+		correct: false
+	}, {
+		text: "0",
+		correct: true
+	}, ]
+}, ];
 
 /* Get the data from respective areas*/
 
@@ -92,12 +139,12 @@ let score = 0;
  * aditionally when an answer is selected 
  * changes the html of "Next"button
  * and calls showQuestion function
-  */
-function startQuiz () {
-    currentQuestionIndex = 0;
-    score = 0;
-    nextButton.innerHTML = "Next";
-    showQuestions();
+ */
+function startQuiz() {
+	currentQuestionIndex = 0;
+	score = 0;
+	nextButton.innerHTML = "Next";
+	showQuestions();
 }
 
 /**
@@ -106,53 +153,53 @@ function startQuiz () {
  * increases the score if correct
  */
 function selectAnswer(e) {
-    const selectedBtn = e.target;
-    const isCorrect = selectedBtn.dataset.correct === "true";
-    if(isCorrect) {
-        selectedBtn.classList.add("correct");
-        score++;
-    }else{
-        selectedBtn.classList.add("incorrect"); 
-    };
-    
-    Array.from(answerButtons.children).forEach(button => {
-        if(button.dataset.correct === "true"){
-            button.classList.add("correct");
-        }
-        button.disabled = true;
-    });
-        nextButton.style.display = "block";
+	const selectedBtn = e.target;
+	const isCorrect = selectedBtn.dataset.correct === "true";
+	if (isCorrect) {
+		selectedBtn.classList.add("correct");
+		score++;
+	} else {
+		selectedBtn.classList.add("incorrect");
+	}
+
+	Array.from(answerButtons.children).forEach(button => {
+		if (button.dataset.correct === "true") {
+			button.classList.add("correct");
+		}
+		button.disabled = true;
+	});
+	nextButton.style.display = "block";
 }
 
 /* Displays the current question and calls the reset function*/
 
 function showQuestions() {
-    resetState();
-    let currentQuestion = questions[currentQuestionIndex];
-    let questionNumber = currentQuestionIndex + 1;
-    questionElement.innerHTML = questionNumber + ". " + currentQuestion.question;
+	resetState();
+	let currentQuestion = questions[currentQuestionIndex];
+	let questionNumber = currentQuestionIndex + 1;
+	questionElement.innerHTML = questionNumber + ". " + currentQuestion.question;
 
-    /* Displays the possible answers to the current question */
-     
-    currentQuestion.answers.forEach(answer => {
-        const button = document.createElement("button");
-        button.innerHTML = answer.text;
-        button.classList.add("btn");
-        answerButtons.appendChild(button);
-        if(answer.correct) {
-            button.dataset.correct = answer.correct;
-        }
-        button.addEventListener("click",selectAnswer);
-    });
+	/* Displays the possible answers to the current question */
+
+	currentQuestion.answers.forEach(answer => {
+		const button = document.createElement("button");
+		button.innerHTML = answer.text;
+		button.classList.add("btn");
+		answerButtons.appendChild(button);
+		if (answer.correct) {
+			button.dataset.correct = answer.correct;
+		}
+		button.addEventListener("click", selectAnswer);
+	});
 }
 
 /*Reset the possible answer for each question */
 
 function resetState() {
-    nextButton.style.display = "none";
-    while(answerButtons.firstChild){ 
-        answerButtons.removeChild(answerButtons.firstChild);
-    };
+	nextButton.style.display = "none";
+	while (answerButtons.firstChild) {
+		answerButtons.removeChild(answerButtons.firstChild);
+	}
 }
 
 /**
@@ -161,14 +208,14 @@ function resetState() {
  * displays the next one (calls de function) or shows the score 
  * if there are no more questions
  */
-    
+
 function nextQuestion() {
-    currentQuestionIndex ++;
-    if(currentQuestionIndex < questions.length){
-        showQuestions();
-    }else{
-        showScore();
-    }
+	currentQuestionIndex++;
+	if (currentQuestionIndex < questions.length) {
+		showQuestions();
+	} else {
+		showScore();
+	}
 }
 
 /**
@@ -177,26 +224,25 @@ function nextQuestion() {
  */
 
 function showScore() {
-    resetState();
-    questionElement.innerHTML = `You got ${score} of ${questions.length}`;
-    nextButton.innerHTML = "Replay";
-    nextButton.style.display = "block"
+	resetState();
+	questionElement.innerHTML = `You got ${score} of ${questions.length}`;
+	nextButton.innerHTML = "Replay";
+	nextButton.style.display = "block";
 }
 
 /** Next button displays the next question if 
  * there are still questions or resets the quiz if 
  * the question was the last of all
-*/
+ */
 
 nextButton.addEventListener("click", () => {
-    if(currentQuestionIndex < questions.length){
-        nextQuestion()
-    }else{
-        startQuiz();
-    }
-}
-)
+	if (currentQuestionIndex < questions.length) {
+		nextQuestion();
+	} else {
+		startQuiz();
+	}
+});
 
 
 
-startQuiz ();
+startQuiz();
