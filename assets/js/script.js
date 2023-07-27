e1cfd76bdec5874b8d8550f0015ce9306f3a422e
GeorgiKostadinovv <>
2023-07-27 17:04:28 +0000

    Add coments to js and html files

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,5 @@
-console.log("conected")
+/* Variable that stores all the questions, possible answers and correct one*/
+
 const questions = [
     {
         question: "How much is 1 + 1?",
@@ -72,28 +73,67 @@ const questions = [
             {text:"0", correct: true},
         ]
     },
-];
+]
+
+/* Get the data from respective areas*/
 
 const questionElement = document.getElementById("question");
 const answerButtons = document.getElementById("answer-buttons");
 const nextButton = document.getElementById("next-btn");
 
+/* Store score and question index */
+
 let currentQuestionIndex = 0;
 let score = 0;
 
+/**
+ * Starts the game and
+ * resets values of question index and score
+ * aditionally when an answer is selected 
+ * changes the html of "Next"button
+ * and calls showQuestion function
+  */
 function startQuiz () {
     currentQuestionIndex = 0;
     score = 0;
     nextButton.innerHTML = "Next";
     showQuestions();
-};
+}
+
+/**
+ * Stores the selected answer and checks if it is correct
+ * also disables the rest of possible answers and
+ * increases the score if correct
+ */
+function selectAnswer(e) {
+    const selectedBtn = e.target;
+    const isCorrect = selectedBtn.dataset.correct === "true";
+    if(isCorrect) {
+        selectedBtn.classList.add("correct");
+        score++;
+    }else{
+        selectedBtn.classList.add("incorrect"); 
+    };
+    
+    Array.from(answerButtons.children).forEach(button => {
+        if(button.dataset.correct === "true"){
+            button.classList.add("correct");
+        }
+        button.disabled = true;
+    });
+        nextButton.style.display = "block";
+}
+
+/* Displays the current question and calls the reset function*/
 
 function showQuestions() {
     resetState();
     let currentQuestion = questions[currentQuestionIndex];
-    let questionNo = currentQuestionIndex + 1;
-    questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
+    let questionNumber = currentQuestionIndex + 1;
+    questionElement.innerHTML = questionNumber + ". " + currentQuestion.question;
 
+    /* Displays the possible answers to the current question */
+     
     currentQuestion.answers.forEach(answer => {
         const button = document.createElement("button");
         button.innerHTML = answer.text;
@@ -104,43 +144,24 @@ function showQuestions() {
         }
         button.addEventListener("click",selectAnswer);
     });
-};
+}
 
+/*Reset the possible answer for each question */
 
 function resetState() {
     nextButton.style.display = "none";
     while(answerButtons.firstChild){ 
         answerButtons.removeChild(answerButtons.firstChild);
     };
-};
-
-function selectAnswer(e) {
-    const selectedBtn = e.target;
-    const isCorrect = selectedBtn.dataset.correct === "true";
-    if(isCorrect) {
-        selectedBtn.classList.add("correct");
-        score++;
-    }else{
-        selectedBtn.classList.add("incorrect"); 
-    };
-    Array.from(answerButtons.children).forEach(button => {
-        if(button.dataset.correct === "true"){
-            button.classList.add("correct");
-        }
-        button.disabled = true;
-    });
-        nextButton.style.display = "block";
-};
-
-nextButton.addEventListener("click", () => {
-    if(currentQuestionIndex < questions.length){
-        nextQuestion();
-    }else{
-        startQuiz();
-    }
 }
-)
 
+/**
+ * Increases the question index and if
+ * there are still more questions then 
+ * displays the next one (calls de function) or shows the score 
+ * if there are no more questions
+ */
+    
 function nextQuestion() {
     currentQuestionIndex ++;
     if(currentQuestionIndex < questions.length){
@@ -150,11 +171,30 @@ function nextQuestion() {
     }
 }
 
+/**
+ * Shows the score, reste it and changes the Next 
+ * button to Replay
+ */
+
 function showScore() {
     resetState();
     questionElement.innerHTML = `You got ${score} of ${questions.length}`;
     nextButton.innerHTML = "Replay";
     nextButton.style.display = "block"
 }
+
+/** Next button displays the next question if 
+ * there are still questions or resets the quiz if 
+ * the question was the last of all
+*/
+
+nextButton.addEventListener("click", () => {
+    if(currentQuestionIndex < questions.length){
+        nextQuestion()
+    }else{
+        startQuiz();
+    }
+}
+)
 
 startQuiz ();
